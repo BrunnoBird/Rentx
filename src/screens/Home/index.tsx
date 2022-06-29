a05a481d2220a73ba44f13c46486d2d901edeff4
brunnobird <bird.tec.gonzalez@gmail.com>
2022-06-29 20:23:44 -0300

    feat: animação no FAB button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet, BackHandler } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Ionicons } from '@expo/vector-icons';
+import { RectButton, PanGestureHandler } from 'react-native-gesture-handler'
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring
+} from 'react-native-reanimated';
+
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton)
 
 import Logo from '../../assets/logo.svg';
 import api from '../../services/api';
@@ -16,7 +26,6 @@ import {
   HeaderContent,
   TotalCars,
   CarList,
-  MyCarsButton,
 } from './styles';
 import { CarDTO } from '../../dtos/CarDTO';
 import { useTheme } from 'styled-components/native';
@@ -26,6 +35,35 @@ export function Home() {
   const theme = useTheme();
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [load, setLoad] = useState(true);
+
+  const positionY = useSharedValue(0);
+  const positionX = useSharedValue(0);
+
+  const myCarsButtonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: positionX.value },
+        { translateY: positionY.value },
+      ]
+    }
+  });
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, ctx: any) {
+      //armazenar o context da posição de onde ele estava
+      ctx.positionX = positionX.value;
+      ctx.positionY = positionY.value;
+    },
+    onActive(event, ctx: any) {
+      //Quando ele esta arrastando
+      positionX.value = ctx.positionX + event.translationX;
+      positionY.value = ctx.positionY + event.translationY;
+    },
+    onEnd() {
+      positionX.value = withSpring(0);
+      positionY.value = withSpring(0);
+    }
+  });
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate("CarDetails", { car });
@@ -47,8 +85,15 @@ export function Home() {
       }
     }
 
-
     fetchCars();
+  }, []);
+
+  useEffect(() => {
+    //Travando o usuario de dar back quando ele esta na tela de HOME no ANDROID
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      return true;
+    });
+    //No iOS temos que fazer lá nas nossas rotas nas options travar o gesture: false
   }, []);
 
   return (
@@ -64,9 +109,11 @@ export function Home() {
             width={RFValue(108)}
             height={RFValue(12)}
           />
-          <TotalCars>
-            Total de {cars.length} carros
-          </TotalCars>
+          {
+            !load && <TotalCars>
+              Total de {cars.length} carros
+            </TotalCars>
+          }
         </HeaderContent>
       </Header>
 
@@ -82,15 +129,39 @@ export function Home() {
         />
       }
 
-      <MyCarsButton
-        onPress={handleOpenMyCars}
-      >
-        <Ionicons 
-          name="ios-car-sport"
-          size={32}
-          color={theme.colors.shape}
-        />
-      </MyCarsButton>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            myCarsButtonStyle,
+            {
+              position: 'absolute',
+              bottom: 13,
+              right: 22
+            }
+          ]}
+        >
+          <ButtonAnimated
+            onPress={handleOpenMyCars}
+            style={[styles.button, { backgroundColor: theme.colors.main }]}
+          >
+            <Ionicons
+              name="ios-car-sport"
+              size={32}
+              color={theme.colors.shape}
+            />
+          </ButtonAnimated>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
